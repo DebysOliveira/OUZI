64e531c73ac0f15079f9ee68925563f2f7eec682
deboradeoliveira2003 <deboradeoliveira2003@gmail.com>
2024-10-31 18:19:14 +0000

    correção de teste

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -6,7 +6,7 @@
     <nav class="bg-white py-10">
     <div class="container mx-auto text-center text-sm">
         <ul class="flex justify-center space-x-8">
-            <li><a class="text-black nav-link" href="/sobre">SOBRE2</a></li>
+            <li><a class="text-black nav-link" href="/sobre">SOBRE</a></li>
             <li><a class="text-black nav-link" href="/projetos">PROJETOS</a></li>
             <li><a class="text-black nav-link" href="/produtos">PRODUTOS</a></li>
             <li><a class="text-black nav-link" href="/blog">BLOG</a></li>
